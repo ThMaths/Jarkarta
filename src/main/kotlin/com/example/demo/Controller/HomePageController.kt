7ae30b01e2e67c425637aa383f901ba62c471801
Evvilly <willybeller@hotmail.fr>
2024-07-23 14:44:00 +0200

    update HomePageController + gitignore

--- src/main/kotlin/com/example/demo/Controller/HomePageController.kt
+++ src/main/kotlin/com/example/demo/Controller/HomePageController.kt
@@ -1,11 +1,11 @@
 package com.example.demo.Controller
 
 import com.example.demo.Service.MatchService
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class HomePageController(val matchService: MatchService) {
 
     //http://localhost:8080/home
