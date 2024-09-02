21ea2af7f2c67ab4e5a818aa05ac95dc8718a359
Evvilly <willybeller@hotmail.fr>
2024-09-02 09:39:59 +0200

    Update AdminPageController.kt

--- src/main/kotlin/com/example/demo/Controller/AdminPageController.kt
+++ src/main/kotlin/com/example/demo/Controller/AdminPageController.kt
@@ -3,6 +3,7 @@ package com.example.demo.Controller
 import com.example.demo.Service.MatchService
 import jakarta.servlet.http.HttpSession
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
@@ -12,7 +13,8 @@ class AdminPageController (val matchService: MatchService){
 
     //http://localhost:8080/adminPage
     @GetMapping("/adminPage")
-    fun showAdminPage( redirectAttributes: RedirectAttributes, session: HttpSession): String {
+    fun showAdminPage( redirectAttributes: RedirectAttributes, session: HttpSession, model: Model): String {
+        model.addAttribute("allMatchs", matchService.getAll())
         return if (session.getAttribute("loggedIn") != null && session.getAttribute("loggedIn") as Boolean) {
             "adminPage"
         } else {
